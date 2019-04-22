83973a7cda80551d406eb1fdfc6fb3f757f32811
Ai1996Himanshu <himanshu.ai1996@gmail.com>
2019-04-22 14:24:40 +0530

    hello

--- app/src/main/java/com/arya/medicine/med_detail.java
+++ app/src/main/java/com/arya/medicine/med_detail.java
@@ -23,4 +23,68 @@ public class med_detail {
         this.medUrl = medUrl;
         this.billUrl = billUrl;
     }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getManData() {
+        return manData;
+    }
+
+    public void setManData(String manData) {
+        this.manData = manData;
+    }
+
+    public String getExpData() {
+        return expData;
+    }
+
+    public void setExpData(String expData) {
+        this.expData = expData;
+    }
+
+    public String getbNo() {
+        return bNo;
+    }
+
+    public void setbNo(String bNo) {
+        this.bNo = bNo;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getMedUrl() {
+        return medUrl;
+    }
+
+    public void setMedUrl(String medUrl) {
+        this.medUrl = medUrl;
+    }
+
+    public String getBillUrl() {
+        return billUrl;
+    }
+
+    public void setBillUrl(String billUrl) {
+        this.billUrl = billUrl;
+    }
 }
--- app/src/main/java/com/arya/medicine/photo.java
+++ app/src/main/java/com/arya/medicine/photo.java
@@ -1,16 +1,31 @@
 package com.arya.medicine;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class photo extends AppCompatActivity {
 
     Button sellBtn;
     RecyclerView items;
+    List<med_detail> medList = new ArrayList<>();
+    MedAdapter medAdapter;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +34,10 @@ public class photo extends AppCompatActivity {
 
         sellBtn = findViewById(R.id.sell_btn);
         items = findViewById(R.id.item_rv);
+        medAdapter = new MedAdapter(medList, this);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        items.setLayoutManager(layoutManager);
+        items.setAdapter(medAdapter);
 
         sellBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +46,24 @@ public class photo extends AppCompatActivity {
                 finish();
             }
         });
+
+        prepareData();
+    }
+
+    private void prepareData() {
+        DatabaseReference db = FirebaseDatabase.getInstance().getReference();
+        ValueEventListener medListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                med_detail med = dataSnapshot.getValue(med_detail.class);
+                Log.d("nuclode", med.getDetail());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("nuclode", databaseError.getMessage());
+            }
+        };
+        db.addValueEventListener(medListener);
     }
 }
